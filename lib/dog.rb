9a4cd914df0a4b48fdd8cb14481ac93a9ceef52a
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-20 06:33:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,5 +84,20 @@ class Dog
     dog
   end
   
+  def self.find_by_name
+    sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE name = ?
+     LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+  end
   
 end
